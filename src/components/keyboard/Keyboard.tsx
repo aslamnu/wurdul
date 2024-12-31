a5b6067296ddb3abd7aed64f8d3138c1cf6a18b7
wurdul <nadeemuaslam@gmail.com>
2024-12-31 11:46:57 +0000

    Add files via upload

---
+++ src/components/keyboard/Keyboard.tsx
@@ -0,0 +1,106 @@
+import { getStatuses } from '../../lib/statuses'
+import { Key } from './Key'
+import { useEffect } from 'react'
+import { ENTER_TEXT, DELETE_TEXT } from '../../constants/strings'
+
+type Props = {
+  onChar: (value: string) => void
+  onDelete: () => void
+  onEnter: () => void
+  guesses: string[]
+  isRevealing?: boolean
+}
+
+export const Keyboard = ({
+  onChar,
+  onDelete,
+  onEnter,
+  guesses,
+  isRevealing,
+}: Props) => {
+  const charStatuses = getStatuses(guesses)
+
+  const onClick = (value: string) => {
+    if (value === 'ENTER') {
+      onEnter()
+    } else if (value === 'DELETE') {
+      onDelete()
+    } else {
+      onChar(value)
+    }
+  }
+
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.code === 'Enter') {
+        onEnter()
+      } else if (e.code === 'Backspace') {
+        onDelete()
+      } else {
+        const key = e.key.toUpperCase()
+        if (key.length === 1 && key >= 'A' && key <= 'Z') {
+          onChar(key)
+        }
+      }
+    }
+    window.addEventListener('keyup', listener)
+    return () => {
+      window.removeEventListener('keyup', listener)
+    }
+  }, [onEnter, onDelete, onChar])
+
+  return (
+    <div>
+      <div className="flex justify-center mb-1">
+        {['ح', 'چ', 'ج', 'ث', 'ٹ', 'ت', 'پ', 'ب', 'ا', 'آ'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center mb-1">
+        {['ص', 'ش', 'س', 'ژ', 'ز', 'ڑ', 'ر', 'ذ', 'ڈ', 'د'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center mb-1">
+        {['ل', 'گ', 'ک', 'ق', 'ف', 'غ', 'ع', 'ظ', 'ط', 'ض'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <Key width={62} value="ENTER" onClick={onClick}>
+          {ENTER_TEXT}
+        </Key>
+        {['ے', 'ی', 'ء', 'ھ', 'ہ', 'و', 'ن', 'م'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+        <Key width={62} value="DELETE" onClick={onClick}>
+          {DELETE_TEXT}
+        </Key>
+      </div>
+    </div>
+  )
+}
---
+++ src/constants/strings.ts
@@ -0,0 +1,24 @@
+export const GAME_TITLE = 'LAFZLE'
+
+export const WIN_MESSAGES = [
+  'Mubarakan!',
+  'Mubarak ho!',
+  'Zabardast!',
+  'Wahh! Kiya baat hai aap ki!',
+]
+export const GAME_COPIED_MESSAGE = 'Game ko clipboard pe copy kr diya gaya hai'
+export const ABOUT_GAME_MESSAGE = 'Game k baray mai mazeed tafseel'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Mazeed alfaaz daaliye'
+export const WORD_NOT_FOUND_MESSAGE = 'Ye lafz humein nhi milla'
+export const CORRECT_WORD_MESSAGE = (solution: string) =>
+  `Lafz ${solution} hai!`
+export const ENTER_TEXT = 'Enter'
+export const DELETE_TEXT = 'Delete'
+export const STATISTICS_TITLE = 'Statistics'
+export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
+export const NEW_WORD_TEXT = 'Agla lafz itni der mai aye ga'
+export const SHARE_TEXT = 'Share'
+export const TOTAL_TRIES_TEXT = 'Qul koshishain'
+export const SUCCESS_RATE_TEXT = 'Qamiyabi ki sharah'
+export const CURRENT_STREAK_TEXT = 'Mojuda '
+export const BEST_STREAK_TEXT = 'Behtareen '
